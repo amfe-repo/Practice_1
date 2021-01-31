873e3a1f13fa5098d964ff050a0c310853d1e467
Akme <Akme@gmail.com>
2021-01-31 00:17:09 -0400

    Segunda version: programas 3 y 4

--- Program.cs
+++ Program.cs
@@ -5,38 +5,12 @@ namespace ConsoleApps
 {
     class Program
     {   
-        static void WriteWelcome(){
-
-            string name = "", str = "";
-
-            Console.WriteLine("Escribeme tu nombre: ");
-
-            name = Console.ReadLine();
-            str = $"\nBienvenido {name}!!";
-
-            Console.WriteLine(str);
-        }
-
-        static void OddEvenNumber(float number){
-
-            string str = "";
-
-            if(number % 2 == 0){
-                str = $"El numero {number} es par"; 
-
-            }else{
-                str = $"El numero {number} es impar";
-
-            }
-
-            Console.WriteLine(str);
-        }
-
-
+        
         static void Main(string[] args){
             //WriteWelcome(); //Programa #1
-            //OddEvenNumber(number: 12.0f); //Programa #2
-
+            //OddEvenNumber(number: 12); //Programa #2
+            //EvaluateNumbers(n2: 65, n1: 45);
+            //ReturnBooleanExpression();
         }
 
         
