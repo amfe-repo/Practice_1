596001a713502c93cb0a69f8cb8841ae14844501
Akme <Akme@gmail.com>
2021-01-30 23:36:55 -0400

    Primera version: programas 1 y 2

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace ConsoleApps
+{
+    class Program
+    {   
+        static void WriteWelcome(){
+
+            string name = "", str = "";
+
+            Console.WriteLine("Escribeme tu nombre: ");
+
+            name = Console.ReadLine();
+            str = $"\nBienvenido {name}!!";
+
+            Console.WriteLine(str);
+        }
+
+        static void OddEvenNumber(float number){
+
+            string str = "";
+
+            if(number % 2 == 0){
+                str = $"El numero {number} es par"; 
+
+            }else{
+                str = $"El numero {number} es impar";
+
+            }
+
+            Console.WriteLine(str);
+        }
+
+
+        static void Main(string[] args){
+            //WriteWelcome(); //Programa #1
+            //OddEvenNumber(number: 12.0f); //Programa #2
+
+        }
+
+        
+    }
+}
